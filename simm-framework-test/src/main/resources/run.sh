32b459a5ac536d885be83424a3f8b8559f4a4bb9
siman <simanman@shixh.com>
2020-09-01 15:44:38 +0800

    docker部署

--- simm-framework-test/src/main/resources/run.sh
+++ simm-framework-test/src/main/resources/run.sh
@@ -7,4 +7,6 @@ echo "制作镜像"
 docker build -t ocr-api:v1.0 .
 
 echo "运行镜像"
-docker run -p 8080:8080 --name="ocr-api" ocr-api:v1.0
+docker run -it -v /usr/tessdata:/usr/tessdata -p 8080:8080 --name="ocr-api" ocr-api:v1.0
+
+# sed -i "s/\r//" run.sh 执行此命令进行转格式
