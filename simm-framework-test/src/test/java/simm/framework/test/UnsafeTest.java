1aa503a7e9e02d13a15e9dfedb360859abdd1966
siman <simanman@shixh.com>
2020-09-24 17:26:06 +0800

    springboot-单元测试

--- simm-framework-test/src/test/java/simm/framework/test/UnsafeTest.java
+++ simm-framework-test/src/test/java/simm/framework/test/UnsafeTest.java
@@ -9,12 +9,19 @@ import java.lang.reflect.Field;
  * 直接分配内存测试
  */
 public class UnsafeTest {
-    private static  final int _1MB = 1024*1024;
+    private static final int _1MB = 1024 * 1024;
+
+    /**
+     * java.lang.OutOfMemoryError 异常测试
+     * @throws Exception
+     */
     @Test
     public void allocate() throws Exception {
         Field unsafeField = Unsafe.class.getDeclaredFields()[0];
         unsafeField.setAccessible(true);
-        Unsafe unsafe = (Unsafe)unsafeField.get(null);
-        unsafe.allocateMemory(_1MB);
+        Unsafe unsafe = (Unsafe) unsafeField.get(null);
+        while (true) {
+            unsafe.allocateMemory(_1MB);
+        }
     }
 }
