16dd55cef7b5d1f6038b1e08ef7944a0d0f37b3b
siman <simanman@shixh.com>
2020-09-24 11:51:16 +0800

    搜集大量的配置信息

--- simm-framework-webutil/src/main/java/simm/framework/webutil/config/LimitInterceptor.java
+++ simm-framework-webutil/src/main/java/simm/framework/webutil/config/LimitInterceptor.java
@@ -82,10 +82,12 @@ public class LimitInterceptor {
                 key = StringUtils.upperCase(method.getName());
         }
         ImmutableList<String> keys = ImmutableList.of(String.format("%s%s",limitAnnotation.prefix(), key));
+        boolean redisSucc = false;
         try {
             String luaScript = buildLuaScript();
             RedisScript<Number> redisScript = new DefaultRedisScript<>(luaScript, Number.class);
             Number count = (Number)redisLuaScript.eval(luaScript,keys, Arrays.asList(String.valueOf(limitCount),String.valueOf(limitPeriod)));
+            redisSucc = true;
 //            Number count = (Number) redisTemplate.execute(redisScript, keys, limitCount,limitPeriod);
             logger.info("Access try count is {} for name={} and key = {}", count, name, key);
             if (count != null && count.intValue() <= limitCount) {
@@ -98,11 +100,16 @@ public class LimitInterceptor {
                 throw new RuntimeException(e.getLocalizedMessage());
             }
             throw new RuntimeException("server exception");
+        }finally {
+            if(redisSucc){
+                String luaScript = buildDecrLuaScript();
+                redisLuaScript.eval(luaScript,keys, Arrays.asList(String.valueOf(limitCount),String.valueOf(limitPeriod)));
+            }
         }
     }
 
     /**
-     * 限流 脚本
+     * 限流脚本(进入)
      *
      * @return lua脚本
      */
@@ -116,10 +123,29 @@ public class LimitInterceptor {
         lua.append("\nend");
         // 执行计算器自加
         lua.append("\nc = redis.call('incr',KEYS[1])");
-        lua.append("\nif tonumber(c) == 1 then");
-        // 从第一次调用开始限流，设置对应键值的过期
-        lua.append("\nredis.call('expire',KEYS[1],ARGV[2])");
+//        lua.append("\nif tonumber(c) == 1 then");
+//        // 从第一次调用开始限流，设置对应键值的过期
+//        lua.append("\nredis.call('expire',KEYS[1],ARGV[2])");
+//        lua.append("\nend");
+        lua.append("\nreturn c;");
+        return lua.toString();
+    }
+
+    /**
+     * 限流 脚本（退出）
+     *
+     * @return lua脚本
+     */
+    public String buildDecrLuaScript() {
+        StringBuilder lua = new StringBuilder();
+        lua.append("local c");
+        lua.append("\nc = redis.call('get',KEYS[1])");
+        // 调用不超过最大值，则直接返回
+        lua.append("\nif c and tonumber(c)== 0 then");
+        lua.append("\nreturn c;");
         lua.append("\nend");
+        // 执行计算器自加
+        lua.append("\nc = redis.call('decr',KEYS[1])");
         lua.append("\nreturn c;");
         return lua.toString();
     }
