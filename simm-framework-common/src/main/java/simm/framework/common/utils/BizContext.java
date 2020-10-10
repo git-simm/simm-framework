1eeb489ee7b16afd99e4a18743e8f684b2c85033
siman <simanman@shixh.com>
2020-10-10 11:22:06 +0800

    springboot-单元测试

---
+++ simm-framework-common/src/main/java/simm/framework/common/utils/BizContext.java
@@ -0,0 +1,68 @@
+package simm.framework.common.utils;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 业务上下文,存储业务信息
+ */
+public class BizContext {
+
+    private static ThreadLocal<Map<String, Object>> local = new ThreadLocal<>();
+
+    /**
+     * 设置值
+     * @param key
+     * @param value
+     * @param <V>
+     */
+    public static <V> void putValue(String key, V value) {
+        if (null == local.get()) {
+            local.set(new HashMap<String, Object>());
+        }
+        Map<String, Object> context = local.get();
+        context.put(key, value);
+    }
+
+//    public static void putUser(AdminUser user) {
+//        putValue("user",user);
+//        putValue("userId",user.getId());
+//        putValue("userName",user.getAdmin_user_name());
+//    }
+//
+//    /**
+//     * 用户
+//     * @return
+//     */
+//    public static AdminUser getUser() {
+//        return getValue("user");
+//    }
+//
+
+    /**
+     * 获取值
+     * @param key
+     * @param <T>
+     * @return
+     */
+    public static <T> T getValue(String key) {
+        if (null == local.get()) {
+            return null;
+        }
+        Map<String, Object> context = local.get();
+        if(!context.containsKey(key)){
+        	return null;
+        }
+        return (T) context.get(key);
+    }
+
+    /**
+     * 清空
+     */
+    public static void clear() {
+        if (null != local.get()) {
+            local.set(null);
+        }
+    }
+}
--- simm-framework-test/src/test/java/simm/framework/test/UnsafeTest.java
+++ simm-framework-test/src/test/java/simm/framework/test/UnsafeTest.java
@@ -5,12 +5,15 @@ import org.junit.Test;
 import sun.misc.Unsafe;
 
 import java.lang.reflect.Field;
+import java.util.HashMap;
 
 /**
  * 直接分配内存测试
  */
 public class UnsafeTest {
     private static final int _1MB = 1024 * 1024;
+
+    private final static ThreadLocal<HashMap<String,Object>> THREAD_LOCAL = new ThreadLocal<>();
 
     /**
      * java.lang.OutOfMemoryError 异常测试
@@ -34,6 +37,20 @@ public class UnsafeTest {
         byte[] allocation;
         //直接分配在老年代中
         allocation = new byte[4*_1MB];
+        Assert.isTrue(true);
+    }
+
+    /**
+     * 对象分配
+     */
+    @Test
+    public void testTenuringThreshold(){
+        byte[] allocation1,allocation2,allocation3;
+        allocation1 = new byte[_1MB/4];
+        allocation2 = new byte[4*_1MB];
+        allocation3 = new byte[4*_1MB];
+        allocation3 = null;
+        allocation3 = new byte[4*_1MB];
         Assert.isTrue(true);
     }
 }
