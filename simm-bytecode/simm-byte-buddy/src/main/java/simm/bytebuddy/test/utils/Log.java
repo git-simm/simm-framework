ff87a9ab9f70877b8ca224b51e039208a8488df7
git-simm <214382122@qq.com>
2021-01-04 16:47:01 +0800

    bytebuddy测试

---
+++ simm-bytecode/simm-byte-buddy/src/main/java/simm/bytebuddy/test/utils/Log.java
@@ -0,0 +1,7 @@
+package simm.bytebuddy.test.utils;
+
+public class Log {
+    public static void log(String a) {
+        System.out.println("Log: " + a);
+    }
+}
---
+++ simm-bytecode/simm-byte-buddy/src/main/java/simm/bytebuddy/test/utils/Log4j.java
@@ -0,0 +1,7 @@
+package simm.bytebuddy.test.utils;
+
+public class Log4j {
+    public static void log(String a) {
+        System.err.println("Log4j: " + a);
+    }
+}
