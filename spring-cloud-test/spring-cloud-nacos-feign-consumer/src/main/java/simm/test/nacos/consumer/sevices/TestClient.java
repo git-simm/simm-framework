fea41134eb2ec86789fa35d8a5a4ea396d811764
git-simm <214382122@qq.com>
2021-01-26 17:05:18 +0800

    nacos配置中心的用法探索代码提交

---
+++ spring-cloud-test/spring-cloud-nacos-feign-consumer/src/main/java/simm/test/nacos/consumer/sevices/TestClient.java
@@ -0,0 +1,16 @@
+package simm.test.nacos.consumer.sevices;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient("service-provider")
+public interface TestClient {
+    /**
+     * 输出测试
+     * @param str
+     * @return
+     */
+    @GetMapping(value = "/echo/{str}")
+    String echo(@PathVariable String str);
+}
